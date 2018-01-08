3ca45cb5517b30ee562898118c6f4f140eed945c
Azur_Azabache <tharuka_20@hotmail.com>
2018-01-08 01:43:54 -0500

    mediaquery

--- js/email.js
+++ js/email.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   var $buttonBackward = $('#backward');
   var $btnNext = $('#btnNext');
   var $checkBox = $('#exampleCheck1');
-  var $inputs = $('.form-control');
+  var $input1 = $('#input1');
+  var $input2 = $('#input2');
+  var $input3 = $('#input3');
   //Evento para regresar de página
   $buttonBackward.on('click', function() {
     window.location.href = '../views/verify.html';
   });
 
   //Evento para habilitar el boton NEXT (escribir primero los espacios en blanco y luego marcar el checkbox)
-  $checkBox.on('change', function() {
-    if ($(this).is(':checked') && $inputs.val() !== '') {
+  $checkBox.on('click', function() {
+    if ($(this).is(':checked') && $input1.val() !== '' && $input2.val() !== '' && $input3.val() !== '') {
       $btnNext.prop('disabled', false);
     }
   });
